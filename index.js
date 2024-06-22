f924f9576c04619a3f7f33fa393cf04c33ecbb1f
Tanmay Agrawal <tanmayagcode@gmail.com>
2024-06-22 22:12:39 +0530

    fix: not found error

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ var express = require("express");
 var cors = require("cors");
 require("dotenv").config();
 const multer = require("multer");
-
 const upload = multer();
 var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/public", express.static(process.cwd() + "/public"));
+app.use(express.static(__dirname + "/public/"));
 
 app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
